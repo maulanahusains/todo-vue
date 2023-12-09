efe7060b4246ae9142a09f45126b865c7e62828a
maulanahusains <maulanahusain@proton.me>
2023-12-09 16:31:32 +0700

    feat: add, edit, remove todo done

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,100 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { computed, onMounted, ref } from 'vue';
+
+  let id = 0;
+  const ubahTodo = ref('');
+  const newTodo = ref('');
+  const data = ref([
+    {id: id++, todo: 'belajar', checked: false, edit: false}
+  ]);
+
+  function addTodo() {
+    data.value.push({id: id++, todo: newTodo.value, checked: false, edit: false});
+    newTodo.value = ''
+  }
+
+  function editTodo(todo) {
+    let edited = data.value.find((o, i) => {
+      if(o.id == todo.id) {
+        data.value[i] = {id: data.value[i].id, todo: data.value[i].todo, checked: data.value[i].checked, edit: true}
+        ubahTodo.value = data.value[i].todo;
+        return true;
+      }
+    }) 
+  }
+
+  function updateTodo(todo) {
+    data.value.find((o, i) => {
+      if(o.id == todo.id) {
+        data.value[i] = {id: data.value[i].id, todo: ubahTodo.value, checked: data.value[i].checked, edit: false}
+        ubahTodo.value = '';
+        return true;
+      }
+    })
+  }
+
+  function removeTodo(todo) {
+    data.value = data.value.filter((i) => i != todo);
+  }
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h2>Simple to-do list</h2>
+
+  <div class="container">
+    <div>
+      <form class="input" @submit.prevent="addTodo">
+        <input type="text" v-model="newTodo">
+        <button @click="addTodo">Add</button>
+      </form>
+    </div>
+    <div class="lists">
+      <ul>
+        <li v-for="list of data" :key="list.id">
+          <input v-if="!list.edit" type="checkbox" v-model="list.checked">
+          <span v-if="!list.edit" :class="{ done:list.checked }">{{ list.todo }}</span>
+          <div v-if="!list.edit">
+            <button @click="editTodo(list)">E</button>
+            <button @click="removeTodo(list)">X</button>
+          </div>
+          <div v-else>
+            <form @submit.prevent="updateTodo(list)" class="input">
+              <input type="text" v-model="ubahTodo">
+              <button>Save</button>
+            </form>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .container {
+    width: 90%;
+    margin: 0 auto;
+  }
+  .input {
+    display: flex;
+    gap: 16px;
+    justify-content: center;
+    align-items: center;
+  }
+  .done {
+    text-decoration: line-through;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    gap: 4px;
+    display: flex;
+  }
 </style>
+
+
